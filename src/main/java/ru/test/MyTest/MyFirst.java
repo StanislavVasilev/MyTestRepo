45910132e73af561447725f6f896b8a8bd7ca6a1
StanislavVasilev <48835281+StanislavVasilev@users.noreply.github.com>
2019-03-24 11:15:08 +0300

    Chsnges3

--- src/main/java/ru/test/MyTest/MyFirst.java
+++ src/main/java/ru/test/MyTest/MyFirst.java
@@ -3,6 +3,6 @@ package ru.test.MyTest;
 public class MyFirst {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
-		System.out.println("Hello, world!");
+		System.out.println("Hello!");
 	}
 }
