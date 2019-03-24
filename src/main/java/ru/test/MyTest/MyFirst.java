e470b8b388486c449f6070f3aba96cc3d6c8c5c6
StanislavVasilev <48835281+StanislavVasilev@users.noreply.github.com>
2019-03-24 10:54:25 +0300

    Changes

--- src/main/java/ru/test/MyTest/MyFirst.java
+++ src/main/java/ru/test/MyTest/MyFirst.java
@@ -1,4 +1,6 @@
-public class MyFirst{
+package ru.test.MyTest;
+
+public class MyFirst {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 		System.out.println("Hello, world!");
