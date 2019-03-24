5c42336a7b60052aea12a52ea7bdcaddf81ceb7f
StanislavVasilev <48835281+StanislavVasilev@users.noreply.github.com>
2019-03-24 12:29:33 +0300

    changed code settings

--- src/main/java/com/test/MyTest/MyFirstTestClass.java
+++ src/main/java/com/test/MyTest/MyFirstTestClass.java
@@ -1,8 +1,15 @@
 package com.test.MyTest;
 
 public class MyFirstTestClass {
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-		System.out.println("Hello, world!");
-	}
+
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+    System.out.println("Hello, world!");
+  }
+
+  public void hello() {
+    System.out.println("Hello, Stanislav!");
+    System.out.println("Hello, my program");
+  }
+
 }
