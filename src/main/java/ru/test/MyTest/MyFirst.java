e77495e6ef25aff67c7a5b663e39912304e017ba
StanislavVasilev <48835281+StanislavVasilev@users.noreply.github.com>
2019-03-24 11:26:33 +0300

    Minor2

--- src/main/java/ru/test/MyTest/MyFirst.java
+++ src/main/java/ru/test/MyTest/MyFirst.java
@@ -3,8 +3,6 @@ package ru.test.MyTest;
 public class MyFirst {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
-		System.out.println("Hello!");
-		System.out.println("Hello!");
-		System.out.println("Hello!");
+		System.out.println("Hello, world!");
 	}
 }
