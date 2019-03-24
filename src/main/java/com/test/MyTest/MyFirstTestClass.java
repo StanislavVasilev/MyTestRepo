a7f54ecf3d71f0a682917e217088cad4fc928255
StanislavVasilev <48835281+StanislavVasilev@users.noreply.github.com>
2019-03-24 19:50:32 +0300

    new methods

--- src/main/java/com/test/MyTest/MyFirstTestClass.java
+++ src/main/java/com/test/MyTest/MyFirstTestClass.java
@@ -3,13 +3,43 @@ package com.test.MyTest;
 public class MyFirstTestClass {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("user");
+    hello("Stanislav");
+
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 4;
+    double b= 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
+    String h = "Hello, ";
+    String w = "world!";
+    System.out.println(s1(h, w));
+
+    String h1 = "Hello";
+    String w1 = ", ";
+    String e = "world!";
+    System.out.println(s2(h1, w1, e));
+  }
+  public static void hello(String somebody){
+     System.out.println("Hello, " + somebody + "!");
   }
 
-  public void greetings() {
-    System.out.println("Hello, Stanislav!");
-    System.out.println("Hello, my program");
+  public static double area(double len){
+    return len * len;
   }
 
-}
+  public static double area(double a, double b){
+    return a * b;
+  }
+
+  public static String s1(String h, String w){
+    return h + w;
+  }
+  public static String s2(String h, String w, String e){
+    return (h + w + e);
+
+  }
+ }
