c45e08004135f1f4764dea3e50a4279429126f19
StanislavVasilev <48835281+StanislavVasilev@users.noreply.github.com>
2019-03-24 11:20:40 +0300

    Chsnges4

--- src/main/java/ru/test/MyTest/MyFirst.java
+++ src/main/java/ru/test/MyTest/MyFirst.java
@@ -4,5 +4,7 @@ public class MyFirst {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 		System.out.println("Hello!");
+		System.out.println("Hello!");
+		System.out.println("Hello!");
 	}
 }
