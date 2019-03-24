c8816f590986d2f712eb82594426bdd3ed8370eb
StanislavVasilev <48835281+StanislavVasilev@users.noreply.github.com>
2019-03-24 10:43:08 +0300

    Trivial

--- src/main/java/MyFirst.java
+++ src/main/java/MyFirst.java
@@ -1,9 +1,5 @@
 public class MyFirst{
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
-		System.out.println("Hello, ------!");
-		System.out.println("Hello, ------!");
-		System.out.println("Hello, everyBody!");
-		System.out.println("Hello, people everyBody!");
 	}
 }
